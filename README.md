d6e54a2e97868f82c29290a0d19d8503904345c2
Segward <gustask@stud.ntnu.no>
2025-03-21 00:32:28 +0100

    Attempting to find processPath with pid

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ This is a simple dylib injection tool that injects a dylib into a target process
 ### Usage
 
 ```shell
-$ gcc -dynamiclib -o payload.dylib payload.c -lpthread
+$ gcc -dynamiclib -o payload.dylib payload.c -lpthread -ldl -lproc
 $ DYLD_INSERT_LIBRARIES=./payload.dylib ./target
 ```
--- payload.c
+++ payload.c
@@ -3,22 +3,33 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <mach-o/dyld.h>
+#include <libproc.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
 
 void* foo() {
 	while(1) {
-		printf("Name: %s\n", _dyld_get_image_name(0));
+		// Print the PID of the process
+		pid_t pid = getpid();
+		printf("PID: %d\n", pid);
+		
+		// Print the path of the executable
+		char path[1024];
+		int length = proc_pidpath(pid, path, sizeof(path));
+		printf("Path: %s\n", path);
 		sleep(1);
 	}
 }
 
 __attribute((constructor))
 void init() {
+	// Create a thread
 	pthread_t thread;
-	
 	if (pthread_create(&thread, NULL, foo, NULL) != 0) {
 		printf("Error creating thread\n");
 		exit(1);
 	}
-
+	
+	// Detach the thread
 	pthread_detach(thread);	
 }
