b56c48e63b22cd51c1ac546e040f8e41adb7f086
Segward <gustask@stud.ntnu.no>
2025-03-28 20:50:04 +0100

    Found [FLog::Network] with offset found in ghidra

--- payload.c
+++ payload.c
@@ -2,25 +2,29 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
 #include <mach-o/dyld.h>
 
 void* foo() {
-  // Get the numer of images
-  uint32_t count = _dyld_image_count();
-  printf("Number of images: %d\n", count);
-
-  // Print the image names
-  for (uint32_t i = 0; i < count; i++) {
-    const char* name = _dyld_get_image_name(i);
-    printf("Image %d: %s\n", i, name);
+  const char* name = _dyld_get_image_name(1);
+  if (strstr(name, "RobloxPlayer") == NULL) {
+    return NULL;
   }
 
-  // Print the image header
-  for (uint32_t i = 0; i < count; i++) {
-    const struct mach_header* header = _dyld_get_image_header(i);
-    printf("Image %d: %p\n", i, header);
+  const struct mach_header* header = _dyld_get_image_header(1);
+  if (header == NULL) {
+    return NULL;
+  }
+  
+  uintptr_t offset = 0x38d3f1a;
+  uintptr_t address = (uintptr_t)header + offset;
+
+  for (int i = 0; i < 15; ++i) {
+    printf("%c", *(char*)(address + i));
   }
 
+  printf("\n");
   return NULL;
 }
 
---
+++ roblox.sh
@@ -0,0 +1,2 @@
+DYLD_INSERT_LIBRARIES=/Users/gustavskyberg/Home/Programming/DylibInjection/payload.dylib /Applications/Roblox.app/Contents/MacOS/RobloxPlayer
+
