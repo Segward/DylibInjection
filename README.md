f32a574dc3158781bf2e4103dd3fc42dd702c527
Segward <gustask@stud.ntnu.no>
2025-03-29 07:02:16 +0100

    Added lua state, might need to hijack existing one from target

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ## Dylib Injection
 
-The dylib is injected using the `DYLD_INSERT_LIBRARIES` environment variable.
+The dylib is injected using the `DYLD_INSERT_LIBRARIES` environment variable. It uses lua version 5.4.7.
 
 ### Usage
 
 ```shell
-$ gcc -dynamiclib -o payload.dylib payload.c -lpthread -ldl
+$ gcc -dynamiclib -o payload.dylib payload.c -I./lua-5.4.7/src -L./lua-5.4.7/src -llua -lm -ldl -lpthread 
 $ DYLD_INSERT_LIBRARIES=./payload.dylib ./target
 ```
--- payload.c
+++ payload.c
@@ -5,15 +5,39 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <mach-o/dyld.h>
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
 
 void* foo() {
   const char* name = _dyld_get_image_name(1);
   if (strstr(name, "Player") == NULL) {
+    printf("Not Player\n");
     return NULL;
   }
 
   const struct mach_header* header = _dyld_get_image_header(1);
   if (header == NULL) {
+    printf("Failed to get header\n");
+    return NULL;
+  }
+
+  lua_State* luaState = luaL_newstate();
+  if (luaState == NULL) {
+    printf("Failed to create lua state\n");
+    return NULL;
+  }
+  
+  luaL_openlibs(luaState);
+  lua_getglobal(luaState, "print");
+  if (!lua_isfunction(luaState, -1)) {
+    printf("print is not a function\n");
+    return NULL;
+  }
+
+  lua_pushstring(luaState, "Hello World!");
+  if (lua_pcall(luaState, 1, 0, 0) != LUA_OK) {
+    printf("Failed to call print\n");
     return NULL;
   }
 
